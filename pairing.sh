53b08294e39a05ca398c402e5b375b3432092f42
codylane <cody.lane@gmail.com>
2025-04-24 09:08:56 -0600

    updates

--- pairing.sh
+++ pairing.sh
@@ -1,20 +1,6 @@
 MINICONDA_ROOT_URI="https://repo.anaconda.com/miniconda/Miniconda3-latest"
+EXPECTED_MINICONDA_INSTALLER_SHA256=1314b90489f154602fd794accfc90446111514a5a72fe1f71ab83e07de9504a7
 MINICONDA_DEFAULT_PREFIX="${HOME}/miniconda3"
-
-_update_git_module()
-{
-
-  local dir="${1}"
-
-  cd "${1}"
-
-  git fetch --all
-  git pull
-
-  cd -
-
-}
-
 
 err()
 {
@@ -69,32 +55,34 @@ get_os()
   esac
 }
 
+get_sha256_of_file()
+{
+  openssl sha256 "${1}" 2>>/dev/null | awk '{print $2}'
+}
+
 
 download_miniconda()
 {
   get_os
 
-  curl -L "${MINICONDA_ROOT_URI}" -o ${HOME}/miniconda3-latest-installer.sh
+  ACTUAL_MINICONDA_INSTALLER_SHA256="$(get_sha256_of_file ${HOME}/miniconda3-latest-installer.sh)"
+  [ "${ACTUAL_MINICONDA_INSTALLER_SHA256}" == "${EXPECTED_MINICONDA_INSTALLER_SHA256}" ] || curl -L "${MINICONDA_ROOT_URI}" -o ${HOME}/miniconda3-latest-installer.sh
 
   chmod 755 ${HOME}/miniconda3-latest-installer.sh
 }
 
-
 install_miniconda()
 {
-
   [ -f ${HOME}/miniconda3-latest-installer.sh ] || download_miniconda
   [ -d "${MINICONDA_DEFAULT_PREFIX}" ] || ${HOME}/miniconda3-latest-installer.sh -b -u -p "${MINICONDA_DEFAULT_PREFIX}"
 }
-
 
 source_miniconda()
 {
   [ -f ${MINICONDA_DEFAULT_PREFIX}/etc/profile.d/conda.sh ] && . ${MINICONDA_DEFAULT_PREFIX}/etc/profile.d/conda.sh || true
 }
 
-
-install_janus_plugins()
+install_janus()
 {
   local required_packages=
 
@@ -104,88 +92,33 @@ install_janus_plugins()
   command -v ack   >>/dev/null || required_packages="${required_packages} ack"
   command -v rake  >>/dev/null || required_packages="${required_packages} rake"
 
+  [ -d ~/.vim ] || git clone https://github.com/carlhuda/janus ~/.vim
   if [ -n "${required_packages}" ]; then
     echo "Please make sure that you have the following packages installed [${required_packages}]"
     return 1
   fi
 
-  mkdir -p  ${HOME}/.janus/
-  cd ${HOME}/.janus
+  [ -d ~/.janus ] && return 0 || true
 
-  [ -d ansible-vim ]        && _update_git_module ansible-vim        || git clone https://github.com/pearofducks/ansible-vim.git
-  [ -d tabular ]            && _update_git_module tabular            || git clone https://github.com/godlygeek/tabular.git
-  [ -d tcomment_vim ]       && _update_git_module tcomment_vim       || git clone https://github.com/tomtom/tcomment_vim.git
-  [ -d vim-flake8 ]         && _update_git_module vim-flake8         || git clone https://github.com/nvie/vim-flake8.git
-  [ -d vim-puppet ]         && _update_git_module vim-puppet         || git clone https://github.com/rodjek/vim-puppet.git
-  [ -d vim-tmux-navigator ] && _update_git_module vim-tmux-navigator || git clone https://github.com/christoomey/vim-tmux-navigator.git
-  [ -d vim-airline ]        && _update_git_module vim-airline        || git clone https://github.com/vim-airline/vim-airline.git
-  [ -d vim-arduino ]        && _update_git_module vim-arduino        || git clone https://github.com/stevearc/vim-arduino.git
-  [ -d ctrlp ]              && _update_git_module ctrlp              || git clone https://github.com/ctrlpvim/ctrlp.vim.git
+  cd ~/.vim
+  ./bootstrap.sh
+  rm -rf janus/vim/tools/vimcss-color
+  sed -i.bak -e '/css-color/d' janus/submodules.yaml
+  mkdir -p  ~/.janus/
 
-  cat > update-submodules.sh << 'EOS'
-#!/usr/bin/env bash
+  cd ~/.janus
+  [ -d ansible-vim ]        || git clone https://github.com/pearofducks/ansible-vim.git
+  [ -d tabular ]            || git clone https://github.com/godlygeek/tabular.git
+  [ -d tcomment_vim ]       || git clone https://github.com/tomtom/tcomment_vim.git
+  [ -d vim-flake8 ]         || git clone https://github.com/nvie/vim-flake8.git
+  [ -d vim-puppet ]         || git clone https://github.com/rodjek/vim-puppet.git
+  [ -d vim-tmux-navigator ] || git clone https://github.com/christoomey/vim-tmux-navigator.git
+  [ -d vim-airline ]        || git clone  https://github.com/vim-airline/vim-airline.git
 
-BLACK="\033[0;30m"
-BLACKBOLD="\033[1;30m"
-RED="\033[0;31m"
-REDBOLD="\033[1;31m"
-GREEN="\033[0;32m"
-GREENBOLD="\033[1;32m"
-YELLOW="\033[0;33m"
-YELLOWBOLD="\033[1;33m"
-BLUE="\033[0;34m"
-BLUEBOLD="\033[1;34m"
-PURPLE="\033[0;35m"
-PURPLEBOLD="\033[1;35m"
-CYAN="\033[0;36m"
-CYANBOLD="\033[1;36m"
-WHITE="\033[0;37m"
-WHITEBOLD="\033[1;37m"
-
-
-info() {
-    MSG_COLOR="${MSG_COLOR:-${GREENBOLD}}"
-
-    echo -en "${MSG_COLOR}${@}\033[0m"
-    echo
-}
-
-
-update() {
-  find . -maxdepth 1 -type d ! -path '.' -and ! -path '..' | while read directory
-  do
-    pushd "${directory}" >>/dev/null
-
-    info "${GREENBOLD}" "Updating $(basename $directory)...."
-    git pull
-    echo
-    popd >>/dev/null
-  done
-}
-
-
-main() {
-    update
-}
-
-
-## main ##
-main
-EOS
-
-chmod 0755 update-submodules.sh
-./update-submodules.sh
-
-}
-
-
-install_neovim()
-{
-
-  mkdir -p ${HOME}/nvim
-  mkdir -p ${HOME}/.config/nvim
-  ln -sf ${HOME}/.vimrc ${HOME}/nvim/init.vim
-  ln -sf ${HOME}/.vimrc ${HOME}/.config/nvim/init.vim
+  mkdir -p ~/nvim
+  mkdir -p ~/.config/nvim
+  ln -sf ~/.vimrc ~/nvim/init.vim
+  ln -sf ~/.vimrc ~/.config/nvim/init.vim
 
   cd ${HOME}
   curl -kLO https://raw.githubusercontent.com/codylane/dotfiles/master/.vimrc.after
@@ -209,9 +142,12 @@ install_neovim()
     -e 's/^let g:python3_host_prog/" let g:python3_host_prog/g' \
     -e 's/^let g:ruby_host_prog/" let g:ruby_host_prog/g'       \
     -i.bak                                                      \
-    ${HOME}/.vimrc.after
-}
+    ~/.vimrc.after
 
+  # 2024-04-24 added this because these no longer work
+  rm -rf janus/vim/tools/tlib/
+  rm -rf janus/vim/tools/supertab/
+}
 
 install_ruby_build()
 {
@@ -227,7 +163,6 @@ install_ruby_build()
 
   path_contains "${HOME}/ruby-build/bin" || export PATH="${HOME}/ruby-build/bin:${PATH}"
 }
-
 
 # User specific environment
 PATH="$HOME/.local/bin:$HOME/bin:$PATH:${MINICONDA_DEFAULT_PREFIX}/bin"
